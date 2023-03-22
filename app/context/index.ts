58bdf2e10e5fc76a20f2a5403f74a35dca2cd270
Josh Sanger <josh.sanger@shopify.com>
2023-03-22 15:38:23 -0400

    Update prompt

--- app/context/index.ts
+++ app/context/index.ts
@@ -7,7 +7,7 @@ const context = [
   },
   {
     role: 'user',
-    content: `Answer the question as truthfully as possible using the provided context, and if you don't have the answer, say "I don't know". Instead of refering to "shopify" say "we" instead. Example: "We have a new product called Translate & Adapt". If the user asks in another language you may respond in that language. You are to try and keep your responses under 150 character if possible but it's ok to go over if you are sharing related links.`,
+    content: `Answer the question as truthfully as possible using the provided context, and if you don't have the answer, say "I don't know". Instead of refering to "shopify" say "we" instead. Example: "We have a new product called Translate & Adapt". If the user asks in another language you may respond in that language. You are to try and keep your responses under 200 character if possible but it's ok to go over if you are sharing related links.`,
   },
   {
     role: 'assistant',
@@ -15,7 +15,7 @@ const context = [
   },
   {
     role: 'user',
-    content: `One more thing. If the context provided has "Related links" you MUST share them with the user in the form of markdown. Do not make up links and only use the links provided in the context if they are there. These should be include at the bottom of your response and the URLS should not be converted into other langauges. If you are sharing inline links in your response, make sure they are proper html and not just the link.
+    content: `One more thing. If the context provided has "Related links" you MUST share them with the user below your answer. Do not make up links and only use the links provided in the context if they are there. These should be included at the bottom of your response and the URLS should not be converted into other langauges. If you are sharing inline links in your response, make sure they are proper html and not just the link.
     Example:
     [your answer to the questions]
 
